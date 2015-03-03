43de3ade59b92f706048715fb824cac64ad3e5af
sjones1 <ipadfan.jones@googlemail.com>
2015-03-03 21:55:06 +0000

    Finish initial layout and routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,12 @@
 Rails.application.routes.draw do
+
   root 'static_pages#home'
 
-  get 'static_pages/home'
-  get 'static_pages/help'
-  get 'static_pages/about'
+  get 'home' => 'static_pages#home'
+  get 'help' => 'static_pages#help'
+  get 'about' => 'static_pages#about'
+  get 'contact' => 'static_pages#contact'
+  get 'signup' => 'users#new'
+  post 'users/create'
 
 end
---
+++ test/controllers/static_pages_controller_test.rb
@@ -0,0 +1,24 @@
+require 'test_helper'
+
+class StaticPagesControllerTest < ActionController::TestCase
+
+  def setup
+    @base_title = "Ruby on Rails Tutorial Sample App"
+  end
+
+  test "should get home" do
+    get :home
+    assert_response :success
+  end
+
+  test "should get help" do
+    get :help
+    assert_response :success
+  end
+
+  test "should get about" do
+    get :about
+    assert_response :success
+  end
+
+end
