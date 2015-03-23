f63db583b94a8184c5416ee69bbf535243b110d3
sjones1 <ipadfan.jones@googlemail.com>
2015-03-23 23:07:35 +0000

    b

---
+++ db/seeds2.rb
@@ -0,0 +1,43 @@
+# This file should contain all the record creation needed to seed the database with its default values.
+# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
+#
+# Examples:
+#
+#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
+#   Mayor.create(name: 'Emanuel', city: cities.first)
+
+User.create!(name:  "Sam Jones",
+             email: "ipadfan.jones@googlemail.com",
+             password:              "test1234",
+             password_confirmation: "test1234",
+             admin: true,
+             teacher: true)
+
+99.times do |n|
+  name  = Faker::Name.name
+  email = "example-#{n+1}@railstutorial.org"
+  password = "password"
+  User.create!(name:  name,
+               email: email,
+               password:              password,
+               password_confirmation: password)
+end
+
+
+users = User.order(:created_at).take(6)
+50.times do
+  content = Faker::Lorem.sentence(5)
+  title = rand(1...910)
+  users.each { |user| user.microposts.create!(content: content, title: title) }
+end
+
+users = User.all
+user  = users.first
+following = users[2..50]
+followers = users[3..40]
+following.each { |followed| user.follow(followed) }
+followers.each { |follower| follower.follow(user) }
+
+Fundraiser.create!(title: "title",
+                   content: "testing content to see if it works",
+                   date: "20/07/2016")
